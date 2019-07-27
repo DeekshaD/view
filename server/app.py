7123c340d84c979b111b5f0631db2019d70e3930
deeksha <deeksha20194@gmail.com>
2019-07-26 23:40:04 -0400

    add density code

--- server/app.py
+++ server/app.py
@@ -4,21 +4,40 @@ from flask_cors import CORS
 import os, sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import pandas as pd
+import numpy as np
+from sklearn.neighbors.kde import KernelDensity
 import glob
 
 app = Flask(__name__)
 api = Api(app)
 
 class FetchData(Resource):
-    def get(self, date):
+    def get(self, date, feature = 0, bandwidth = 10):
         data = pd.read_csv('../data/Sensor_Weather_Data_Challenge.csv', index_col = 0, parse_dates = True)
         day = {}
-        day = data[date]
-        day_0 = day.iloc[:,0]
-        return day_0.to_json(orient="index")
+        self.feature = int(feature)
+        self.bandwidth = bandwidth
+        df_day = data[date].iloc[:, self.feature]
+        df_month = data[date[0:7]].iloc[:, self.feature]
+        std = df_month.std()
+        X_lim = [df_month.min() - std, df_month.max() + std]
+        self.X_ax = np.linspace(X_lim[0], X_lim[1] ,1000)[:, np.newaxis]
+        #kernel density estimation
+        df_kde = pd.DataFrame(data = {"x" : self.X_ax.flatten(), 
+                      "m" : self.getKDE(df_month), 
+                      "d" : self.getKDE(df_day)})
+        return {"x_lim" : X_lim, "df" : df_kde.to_dict(orient="index")}
+
+    def getKDE(self, df):
+        vals = df.values.reshape((-1,1))
+        kde = KernelDensity(bandwidth = self.bandwidth)
+        kde.fit(vals)
+        prop = kde.score_samples(self.X_ax)
+        return np.exp(prop)
 
 
-api.add_resource(FetchData, '/main/date=<date>')
+api.add_resource(FetchData, '/main/date=<date>',
+                            '/main/date=<date>/feature=<feature>')
 @app.route('/main')
 def selectDate():
     return render_template('index.html')
--- server/static/index.js
+++ server/static/index.js
@@ -12,10 +12,20 @@ jQuery(document).ready(function($){
 });
 
 
-var data = [1,2,3,4]
+var margin = {top: 20, right: 20, bottom: 40, left: 50},
+	width = 800 - margin.left - margin.right,
+	height = 800 - margin.top - margin.bottom;
 
-var margin = {top: 20, right: 20, bottom: 40, left: 50};
-var width = 800;
-var height = 800;
+//append svg object
+var svg = d3.select("#density_plots")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//get data
+
+var data = d3.json(url, function(data){
+console.log(data);})
+//console.log(data)
 
 
