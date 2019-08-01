bf1c6055900cb053b772a7f77a59422816d98d98
deeksha <deeksha20194@gmail.com>
2019-08-01 18:38:36 -0400

    complete density, start arcs

--- server/app.py
+++ server/app.py
@@ -5,28 +5,39 @@ import os, sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import pandas as pd
 import numpy as np
+from scipy import stats
 from sklearn.neighbors.kde import KernelDensity
 import glob
 
 app = Flask(__name__)
 api = Api(app)
 
+data = pd.read_csv('../data/Sensor_Weather_Data_Challenge.csv', index_col = 0, parse_dates = True)
+features = list(data.columns)
+
 class FetchData(Resource):
     def get(self, date, feature = 0, bandwidth = 10):
         data = pd.read_csv('../data/Sensor_Weather_Data_Challenge.csv', index_col = 0, parse_dates = True)
+        features = list(data.columns)
         day = {}
         self.feature = int(feature)
         self.bandwidth = bandwidth
         df_day = data[date].iloc[:, self.feature]
         df_month = data[date[0:7]].iloc[:, self.feature]
+        stats_day = {"min": np.min(df_day),
+                "mean": np.mean(df_day),
+                "median": np.median(df_day),
+                "std": np.std(df_day),
+                "max": np.max(df_day)}
+                
         std = df_month.std()
-        X_lim = [df_month.min() - std, df_month.max() + std]
+        X_lim = [min(df_month.min(), df_day.min())- std, max(df_day.max(), df_month.max()) + std]
         self.X_ax = np.linspace(X_lim[0], X_lim[1] ,1000)[:, np.newaxis]
         #kernel density estimation
         df_kde = pd.DataFrame(data = {"x" : self.X_ax.flatten(), 
                       "m" : self.getKDE(df_month), 
                       "d" : self.getKDE(df_day)})
-        return {"x_lim" : X_lim, "df" : df_kde.to_dict(orient="index")}
+        return {"x_lim" : X_lim, "stats": stats_day, "df" : df_kde.melt(id_vars="x").values.tolist()}
 
     def getKDE(self, df):
         vals = df.values.reshape((-1,1))
@@ -40,6 +51,7 @@ api.add_resource(FetchData, '/main/date=<date>',
                             '/main/date=<date>/feature=<feature>')
 @app.route('/main')
 def selectDate():
-    return render_template('index.html')
+    return render_template('index.html', len = len(features), features = features)
+
 if __name__ == "__main__":
     app.run(debug = True)
--- server/static/index.js
+++ server/static/index.js
@@ -1,31 +1,21 @@
 var url="";
+var date;
 jQuery(document).ready(function($){
 	$('.user-input').on('submit', function(){
-		date = $("[name=date]").val(); 					
-		toks = date.split('/');
-		date = toks[2]+'-'+toks[0]+'-'+toks[1];
-		url = "http://127.0.0.1:5000/main/date="+date;
+		date = $("[name=date]").val(); 				       url = "http://127.0.0.1:5000/main/date="+date;
 		console.log(url);
-		//drawROC(url);
-		return false
+		drawPlot(url);
+		return false;
 	});
+
+	$('#featureSelect').on('change', function(){
+		console.log("here");
+		var f_no = $(this).val();
+		var f_url = "http://127.0.0.1:5000/main/date="+date+"/feature="+f_no;
+		console.log(f_url);
+		drawPlot(f_url);
+		return false;
+	});
+
 });
-
-
-var margin = {top: 20, right: 20, bottom: 40, left: 50},
-	width = 800 - margin.left - margin.right,
-	height = 800 - margin.top - margin.bottom;
-
-//append svg object
-var svg = d3.select("#density_plots")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//get data
-
-var data = d3.json(url, function(data){
-console.log(data);})
-//console.log(data)
-
 
